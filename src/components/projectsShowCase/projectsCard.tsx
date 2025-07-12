a4111296f39aff78f9d96c05f464a5814feb0d3f
Amanbig <amanpreetsinghjhiwant@outlook.com>
2025-07-12 06:25:32 +0530

    removed usused files

--- src/components/projectsShowCase/projectsCard.tsx
+++ src/components/projectsShowCase/projectsCard.tsx
@@ -48,7 +48,7 @@ export function ProjectTestimonials() {
       name: "Gem AI App",
       designation: "Flutter, Dart, AI/ML, Gemini API, Firebase",
       github:"https://github.com/Amanbig/Gemini_app",
-      src: "https://images.unsplash.com/photo-1624561172888-ac93c696e10c?q=80&w=2592&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      src: "https://images.unsplash.com/photo-1624561172888-ac93c696e10c?q=80&w=2592&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB9fHx8fA%3D%3D",
     },
   ];
   return <AnimatedTestimonials testimonials={testimonials} />;
